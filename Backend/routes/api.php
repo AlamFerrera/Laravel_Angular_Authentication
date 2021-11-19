4a0c79d9bb93e1558318352f742855324a71e108
Alam Ferrera <alamferrera@gmail.com>
2021-11-19 15:09:36 -0400

    login added
    login added

--- Backend/routes/api.php
+++ Backend/routes/api.php
@@ -4,8 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::group([
-    'middleware' => 'api',
-    'prefix' => 'auth'
+    'middleware' => 'api'
 ], function(){
     Route::post('login','AuthController@login');
     Route::post('logout','AuthController@logout');
--- Frontend/src/app/components/login/login.component.ts
+++ Frontend/src/app/components/login/login.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  public form = {
+    email:null,
+    password: null
+  };
+  private apiUrl = "http://127.0.0.1:8000/api";
+  public error = null;
+
+  constructor(private http:HttpClient) { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(){
+    return this.http.post(`${this.apiUrl}/login`,this.form)
+            .subscribe(
+              data => console.log(data),
+              error => this.handleError(error)
+            );
+  }
+
+  handleError(error){
+    this.error = error.error.error;
   }
 
 }
